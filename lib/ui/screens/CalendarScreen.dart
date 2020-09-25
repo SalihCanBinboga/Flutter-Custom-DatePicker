d9d7157fb7c4bc884a9bfe70f909a9b0a423b465
salihcan <salihcn.bnb@gmail.com>
2020-09-25 13:25:01 +0300

    Prepare dynamic rows base

--- lib/ui/screens/CalendarScreen.dart
+++ lib/ui/screens/CalendarScreen.dart
@@ -9,98 +9,44 @@ class CalendarScreen extends StatefulWidget {
 
 class _CalendarScreenState extends State<CalendarScreen> {
   DateTime now;
-
   DateTime startDate;
   DateTime lastDate;
+
 
   @override
   void initState() {
     now = DateTime.now();
-
     startDate = DateTime(now.year, now.month, now.day);
     lastDate = now.add(Duration(days: 180));
+
 
     super.initState();
   }
 
+
   @override
   Widget build(BuildContext context) {
+
+
+    List<Widget> dayRows = [];
+    dayRows.add(WeekDaysFirstRow());
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Tarih Seçin"),
       ),
       body: Container(
+        margin: EdgeInsets.only(top: 16),
         color: Colors.pink,
         height: MediaQuery.of(context).size.height * .9,
         width: MediaQuery.of(context).size.width,
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 16,
-            ),
-            WeekDaysFirstRow(),
-            Row(
-              children: <Widget>[
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-                _fullDay(startDate),
-              ],
-            ),
-          ],
-        ),
+        child: Column(children: dayRows),
       ),
     );
   }
 }
 
-Widget _fullDay(DateTime dateDay) {
-  return Expanded(child: Text(dateDay.day.toString(), textAlign: TextAlign.center, style: TextStyle(color: Colors.white)));
-}
+Widget _fullDay(DateTime dateDay) => Expanded(child: Text(dateDay.day.toString(), textAlign: TextAlign.center, style: TextStyle(color: Colors.white)));
+
+Widget get _emptyDay => Expanded(child: Text("boş", textAlign: TextAlign.center, style: TextStyle(color: Colors.white)));
