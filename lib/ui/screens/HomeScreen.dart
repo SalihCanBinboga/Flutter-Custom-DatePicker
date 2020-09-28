38a43620d901dffffa03f281275c23a807d91b77
salihcan <salihcn.bnb@gmail.com>
2020-09-28 12:29:06 +0300

    Result date localization completed

--- lib/ui/screens/HomeScreen.dart
+++ lib/ui/screens/HomeScreen.dart
@@ -1,8 +1,23 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'CalendarScreen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    initializeDateFormatting();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,11 +44,19 @@ class HomeScreen extends StatelessWidget {
   }
 
   _showDate(DateTime dateTime) {
+    var dateStandard = DateFormat("d.M.y",Platform.localeName).format(dateTime);
+    var dateNamedMonth = DateFormat("d MMMM y",Platform.localeName).format(dateTime);
+    var dateShortMonthDay = DateFormat("d MMM y EEEE",Platform.localeName).format(dateTime);
+    var dateShortMonthShortDay = DateFormat("d MMM y E",Platform.localeName).format(dateTime);
+
     _scaffoldKey.currentState.showSnackBar(SnackBar(
       content: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            Text("Gelen Tarih: $dateTime"),
+            Text("Gelen Tarih: $dateStandard"),
+            Text("Gelen Tarih: $dateNamedMonth"),
+            Text("Gelen Tarih: $dateShortMonthDay"),
+            Text("Gelen Tarih: $dateShortMonthShortDay"),
           ],
         ),
       ),
